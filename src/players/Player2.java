cc55d49c03d361ff75483b9d5ed77354975a1fad
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-05-19 12:11:37 -0300

    Create Player2.java

---
+++ src/players/Player2.java
@@ -0,0 +1,14 @@
+package players;
+
+public class Player2 extends Players {
+	public void Player2(int positionLine, int positionCol) {
+		super.positionCol = positionCol;
+		super.positionLine = positionLine;
+	}
+	
+	public void xAction() {
+		System.out.println("x");
+		
+	}
+
+}
