8144f98f073205b71a1ca7dc442cf26def5c2670
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-05-19 12:10:26 -0300

    Create Players.java

---
+++ src/players/Players.java
@@ -0,0 +1,25 @@
+package players;
+
+public class Players {
+	int positionLine;
+	int positionCol;
+	
+	
+	public void setPositionCol(int col) {
+		this.positionCol = col;
+	}
+	
+	public void setPositionLine(int line) {
+		this.positionLine = line;
+	}
+	
+	public int getPositionCol() {
+		return positionCol;
+	}
+	
+	public int getPositionLine() {
+		return positionLine;
+	}
+	
+	
+}
